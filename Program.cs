bd9cc9ac9935cf210f8afff3b2b0d1883b7c84b9
Thurman <rthurman@mpwservices.com>
2018-08-20 10:01:48 -0400

    Helloworld

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloMicroservice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
